e2d3ae4c831e55d21bb4a215e73e3cb8a14b0ed4
Paul Hively <pbhively@gmail.com>
2019-11-27 12:22:02 -0600

    Email survey: more supplemental data
    - Included email address status, change date, etc. from when the survey was sent and today
    - Converted various fields to valid date formats for analysis
    - Aggregated data file rolled up to the entity level
    - Duplicate entity row checks

--- 2019-email-survey-analysis/sql/alumni emails.sql
+++ 2019-email-survey-analysis/sql/alumni emails.sql
@@ -1,7 +1,14 @@
 -- KSM alumni email addresses
 Select
   email.id_number
-  , email_address
+  , email.email_address
+  , email.email_status_code
+  , tms_es.short_desc
+  , email.status_change_date
+  , email.date_modified
+  , email.preferred_ind
 From email
 Inner Join v_entity_ksm_degrees deg
   On deg.id_number = email.id_number
+Left Join tms_email_status tms_es
+  On tms_es.email_status_code = email.email_status_code
